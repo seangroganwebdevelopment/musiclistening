ddc90d8594bc8480167f05cebb84705e61c36cbd
Sean Grogan <seangrogan11@gmail.com>
2023-06-25 10:11:33 +0200

    10.11am 23.06.25

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-const colorButton = document.getElementById('colorButton');
-const body = document.body;
 
-colorButton.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
+
+
+const colorButton1 = document.getElementById('colorButton1');
+let body1 = document.body;
+
+colorButton1.addEventListener('click', () => {
+  // if()
+  body1.classList.toggle('dark-mode');
+});
+
+const colorButton2 = document.getElementById('colorButton2');
+let body2 = document.body;
+
+colorButton2.addEventListener('click', () => {
+  body2.classList.toggle('green-mode');
 });
